b67bfc123b046a146c5b91d9d3befb187eed50e3
oscarcbr <oscarbed@gmail.com>
2021-05-28 14:38:21 +0200

    Add files via upload

---
+++ adrenal_gland_human.d/9c_standarize_gene_name/standarize_gene_name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+#NOTE: This script will standarize the gene names for all study cases 
+#in "index.crrltns.txt" and "index.enrchmnts.txt" databases.
+
+#Execute
+python stndrzGnNm.py
