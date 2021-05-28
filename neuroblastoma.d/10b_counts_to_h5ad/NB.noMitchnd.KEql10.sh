62882ec65b12e0f1a3ec243edbd6b17adae2af6a
oscarcbr <oscarbed@gmail.com>
2021-05-28 15:07:17 +0200

    Add files via upload

---
+++ neuroblastoma.d/10b_counts_to_h5ad/NB.noMitchnd.KEql10.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+##########################
+##########################
+##########################
+#####   Variables   ######
+##########################
+##########################
+##########################
+
+#Paths NB.noMitchnd.KEql10
+raw_cnts='data/NB.noMitchnd.d/KEql10.d/NB.noMitchnd.KEql10.cnts.csv'
+clstrs_PAGODA='data/NB.noMitchnd.d/KEql10.d/NB.noMitchnd.KEql10.clstrsLbld.tsv'
+depot_prefix='NB.noMitchnd.KEql10'
+#Files to make dictionaries
+flSmplStg='data/index.smplsINSSstgs.txt'
+flSmplOutcm='data/index.smplsOutcm.txt'
+#Optional files to include tSNE from PAGODA
+pagoda_tSNEFl='data/NB.noMitchnd.d/KEql10.d/NB.noMitchnd.KEql10.tsne.tsv'
+flippingTsne=FALSE
+flippingHrzntlTsne=TRUE
+
+
+######################
+#First methods to run#
+######################
+#If True will write a h5ad file with the basics counts and PAGODA embedding
+w_bscs=TRUE
+
+python3 counts_to_h5ad.py -b=$w_bscs -P=$pagoda_tSNEFl -F=$flippingTsne -r=$raw_cnts -c=$clstrs_PAGODA -d=$depot_prefix -s=$flSmplStg -O=$flSmplOutcm -H=$flippingHrzntlTsne
